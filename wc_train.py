984a0ca87966b2597901edecdc5ee3cf40e0b160
buck <ndbucksmith@gmail.com>
2019-01-31 09:49:26 -0800

    rnn training maybe acceptably

--- wc_train.py
+++ wc_train.py
@@ -20,12 +20,12 @@ params['batch_size'] = 400
 b_size = params['batch_size']
 params['f_width'] = 12
 
-params['learn_rate'] = 0.3
+params['learn_rate'] = 0.05
 params['init_stddev'] = 0.05
 params['take'] = [2,3,4,5,6,7,8,9,10,11,12,13]
 take = params['take']
 params['x_size'] = len(params['take'])
-params['cell_size'] = 32
+params['cell_size'] = 47
 params['rxin_size'] = 21  # wcs + h1h + lwi + el
 pstr = "traing with: "
 for idx in range(len(params['take'])):
@@ -38,7 +38,7 @@ sess = tf.Session()
 rmdl = gtm.climaRNN(sess, params)
 sess.run(tf.global_variables_initializer())
 
-for mcx in range(1):
+for mcx in range(500):
 
   for tx in range(80):
     start_t = time.time()
@@ -59,12 +59,13 @@ for mcx in range(1):
     fetch = [rmdl.lossers, rmdl.hypos, rmdl.ts, rmdl.y_trues, ]
  
     errs, ests, step, yt   = sess.run(fetch, feed)
-
+    errs =np.array(errs)
     if tx % 100 == 99:
       pass #pdb.set_trace()
-    print(tx, errs,  ests[0].mean(), ests[0].min(), ests[0].max())
+    gtu.arprint([tx, errs.mean(), errs.max(), errs.min(),  ests[0].mean(), ests[0].min(), ests[0].max()])
 
-
+  if errs.mean() < 80.0:
+    pdb.set_trace()
 
   
  # pdb.set_trace()
